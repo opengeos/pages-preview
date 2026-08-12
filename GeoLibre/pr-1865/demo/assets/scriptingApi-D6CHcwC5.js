ddaed12ebf29a078e342671c45b51457f39ba0c9
giswqs <giswqs@users.noreply.github.com>
2026-08-12 17:31:06 +0000

    Deploy preview for PR 1865 🛫

---
+++ GeoLibre/pr-1865/demo/assets/scriptingApi-D6CHcwC5.js
@@ -0,0 +1,1 @@
+import{n as r}from"./scriptingApi-DwZ961A7.js";export{r as createScriptingHandlers};
---
+++ GeoLibre/pr-1865/demo/assets/share-fetch-CEL1BmFv.js
@@ -0,0 +1,1 @@
+import{n as t}from"./share-fetch-BUtA6zad.js";export{t as installNativeShareFetch};
