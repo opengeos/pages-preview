3135da531642cf7ee24ac45985a34b74bdc34d0f
giswqs <giswqs@users.noreply.github.com>
2026-08-20 11:52:08 +0000

    Deploy preview for PR 2015 🛫

---
+++ GeoLibre/pr-2015/demo/assets/layer-refresh-Bp6kQoQt.js
@@ -0,0 +1,1 @@
+import{o}from"./layer-refresh-DBssxkJ5.js";export{o as fetchWfsGeoJson};
---
+++ GeoLibre/pr-2015/demo/assets/src-DTCns5OO.js
@@ -0,0 +1,1 @@
+import"./usePlugins-DfYeUALI.js";import{$u as r,Xu as e,ed as a,qs as o}from"./src-DNyhtigQ.js";import"./App-CzAXHWs5.js";export{e as addArcGISLayer,o as materializeEmbeddableVectorLayers,r as refreshArcGISFeatureLayer,a as reloadArcGISViewportLayer};
