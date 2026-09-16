14901d89cb73887b3e54a49d5a8b0cc720a5e360
giswqs <giswqs@users.noreply.github.com>
2026-09-16 23:50:42 +0000

    Deploy preview for PR 2440 🛫

--- GeoLibre/pr-2440/demo/assets/scriptingApi-ZV3njpmS.js
+++
@@ -1,1 +0,0 @@
-import{n as r}from"./scriptingApi-CjKUcZ2E.js";export{r as createScriptingHandlers};
---
+++ GeoLibre/pr-2440/demo/assets/src-BCdoU8r4.js
@@ -0,0 +1,1 @@
+import{$u as r,ed as e,qu as a,rd as t}from"./src-BKdhnset.js";import"./usePlugins-B_P2Ol2-.js";import{c as o}from"./maplibre-vector-CPtcJJO0.js";import"./App-B2VKjS2F.js";export{a as addArcGISLayer,o as materializeEmbeddableVectorLayers,r as refreshArcGISFeatureLayer,e as reloadArcGISViewportLayer,t as setArcGISFetch};
